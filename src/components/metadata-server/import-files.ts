c1b552f14679aeecffb4929155604e71f1f700ec
Robert Kawecki <robert+github@rkaw.pl>
2022-11-06 19:39:57 +0100

    Merge files into the main entries table

--- src/components/metadata-server/import-files.ts
+++ src/components/metadata-server/import-files.ts
@@ -1,6 +1,5 @@
-import { PoolClient, Pool } from "pg";
+import { PoolClient, Pool, Query } from "pg";
 import { from as copyFrom } from "pg-copy-streams";
-import cuid from "cuid";
 import { FilesystemEntry } from "../../common/filesystem-entry";
 import { Transform } from "node:stream";
 import { pipeline } from "node:stream/promises";
@@ -29,20 +28,49 @@ async function borrow<TReturn>(pool: Pool, clientFunction: (client: PoolClient) 
 }
 
 export async function importFiles(pool: Pool, tableName: string, backendID: number, source: AsyncIterable<FilesystemEntry>) {
-    await borrow(pool, (client) => {
-        return transaction(client, async (trx) => {
-            const importTableName = `import_${cuid()}`;
-            // TODO: CREATE TEMPORARY TABLE ... (LIKE entries) ON COMMIT DROP
-            await trx.query(`CREATE TABLE ${importTableName} (LIKE entries INCLUDING ALL)`);
-            const writable = trx.query(copyFrom(`COPY ${importTableName} (backend_id, path, type, bytes, mtime) FROM STDIN`));
+    await borrow(pool, async (client) => {
+        // Before the transaction, mark our import as started.
+        const importStart = await client.query('INSERT INTO imports (backend_id) VALUES ($1) RETURNING import_id', [ backendID ]);
+        const importID: string = importStart.rows[0].import_id;
+        console.log('Import: %d', importID);
+        await transaction(client, async (trx) => {
+            // Copy all received files into a temporary table:
+            const importTableName = `import_${importID}`;
+            await trx.query(`CREATE TEMPORARY TABLE ${importTableName} (LIKE entries INCLUDING ALL) ON COMMIT DROP`);
+            const writable = trx.query(copyFrom(`COPY ${importTableName} (backend_id, path, type, bytes, mtime, first_discovery_at, last_change_at) FROM STDIN`));
             const toTSV = new Transform({
                 writableObjectMode: true,
                 readableObjectMode: false,
                 transform(entry: FilesystemEntry, _encoding, callback) {
-                    callback(null, `${backendID}\t${entry.path}\t${entry.type}\t${entry.bytes}\t${new Date(entry.mtime).toISOString()}\n`);
+                    callback(null, `${backendID}\t${entry.path}\t${entry.type}\t${entry.bytes}\t${new Date(entry.mtime).toISOString()}\t${importID}\t${importID}\n`);
                 }
             })
             await pipeline(source, toTSV, writable);
+            // Run change detection based on last-modified time (mtime):
+            await trx.query(`MERGE INTO entries AS target
+                USING ${importTableName} AS input
+                ON (input.backend_id = target.backend_id AND input.path = target.path)
+                WHEN NOT MATCHED THEN INSERT (backend_id, path, type, bytes, mtime, first_discovery_at, last_change_at)
+                    VALUES (input.backend_id, input.path, input.type, input.bytes, input.mtime, input.first_discovery_at, input.last_change_at)
+                WHEN MATCHED AND (input.mtime <> target.mtime) THEN UPDATE
+                    SET mtime = input.mtime, bytes = input.bytes, last_change_at = input.last_change_at
+                WHEN MATCHED THEN DO NOTHING
+            `);
+            await trx.query(`UPDATE imports
+                SET
+                    finished_at = $1,
+                    entry_count = (SELECT COUNT(*) FROM ${importTableName}),
+                    new_count = (SELECT COUNT(*) FROM entries WHERE backend_id = $2 AND first_discovery_at = $3),
+                    changed_count = (SELECT COUNT(*) FROM entries WHERE backend_id = $4 AND first_discovery_at <> $5 AND last_change_at = $6)
+                WHERE import_id = $7`, [
+                        new Date(),
+                        backendID,
+                        importID,
+                        backendID,
+                        importID,
+                        importID,
+                        importID
+                    ]);
         });
     })
 }
--- src/components/metadata-server/queries/schema/1667740577_init.sql
+++ src/components/metadata-server/queries/schema/1667740577_init.sql
@@ -6,12 +6,24 @@ CREATE TABLE backends (
   server_url TEXT NOT NULL
 );
 
+CREATE TABLE imports (
+  backend_id INT NOT NULL REFERENCES backends(backend_id),
+  import_id BIGINT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  started_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  finished_at TIMESTAMP WITH TIME ZONE,
+  entry_count BIGINT,
+  new_count BIGINT,
+  changed_count BIGINT
+);
+
 CREATE TABLE entries (
   backend_id INT NOT NULL REFERENCES backends(backend_id),
   path TEXT NOT NULL,
   type entry_type NOT NULL,
   bytes BIGINT NOT NULL,
-  mtime TIMESTAMP WITH TIME ZONE NOT NULL
+  mtime TIMESTAMP WITH TIME ZONE NOT NULL,
+  first_discovery_at BIGINT NOT NULL REFERENCES imports (import_id),
+  last_change_at BIGINT NOT NULL REFERENCES imports(import_id)
 ) PARTITION BY LIST (backend_id);
 
 CREATE UNIQUE INDEX path_index ON entries USING btree (path ASC, backend_id ASC);
