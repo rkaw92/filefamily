66e8ff0db840e81d6548facbee99dfdf459ee796
Robert Kawecki <robert+github@rkaw.pl>
2022-11-06 22:03:36 +0100

    Add indexing of parent dirs for fast listing

--- src/components/metadata-server/queries/schema/1667740577_init.sql
+++ src/components/metadata-server/queries/schema/1667740577_init.sql
@@ -1,5 +1,32 @@
 CREATE TYPE entry_type AS ENUM ('file', 'directory', 'link', 'other');
 
+-- Function copied from https://github.com/elfsternberg/postgres-basename-dirname/blob/master/basename.sql
+-- Copyright ⓒ 2016 Elf M. Sternberg <elf.sternberg@gmail.com>
+CREATE OR REPLACE FUNCTION dirname(fullpath text,
+                                   separator char DEFAULT '/')
+   RETURNS text AS
+$$
+DECLARE
+   separare text;
+   dirname text;
+   compname text;
+BEGIN
+   IF position(separator in fullpath) = 0 THEN
+      return '';
+   END IF;
+   separare := '^(.*' || separator || ').*$';
+   dirname = regexp_replace(fullpath, separare, '\1');
+   IF length(dirname) != 0 THEN
+      compname := lpad('', length(dirname), separator);
+      IF compname != dirname THEN
+         dirname = rtrim(dirname, separator);
+      END IF;
+   END IF;
+   return dirname;
+END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
+
 CREATE TABLE backends (
   backend_id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
   backend_name TEXT NOT NULL CONSTRAINT unique_backend_name UNIQUE,
@@ -27,4 +54,5 @@ CREATE TABLE entries (
   last_change_at BIGINT NOT NULL REFERENCES imports(import_id)
 ) PARTITION BY LIST (backend_id);
 
-CREATE UNIQUE INDEX path_index ON entries USING btree (path ASC, backend_id ASC);
+CREATE UNIQUE INDEX path_index ON entries USING btree (path text_pattern_ops ASC, backend_id ASC);
+CREATE INDEX entries_path_dirname ON entries (dirname(path));
